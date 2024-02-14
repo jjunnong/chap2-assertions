b556ce6a19afa2fa2832c90dff52d67355aefe82
DESKTOP-E9CUUFE\804-19 <cacao3216@gmail.com>
2024-02-14 15:31:26 +0900

    Initial commit

---
+++ src/main/java/com/ohgiraffers/assertions/section01/jupiter/Calculator.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.assertions.section01.jupiter;
+
+public class Calculator {
+    public int plusTwoNumber(int firstNum, int secondNum){
+        return firstNum + secondNum;
+    }
+}
